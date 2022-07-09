b525abeb3a091e4a89805b49d8743d7516ec373a
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-07-09 12:50:36 +0600

    update

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ async function run() {
     //Single product Find
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
       const query = { _id: ObjectId(id) };
       const result = await productCollection.findOne(query);
       res.send(result);
